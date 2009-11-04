d6a56402a42af7bd27a69af2c63ec80d703478d4
Hugo Wetterberg <hugo.wetterberg@gmail.com>
2009-11-04 10:08:49 +0100

    Restyled the context listing

--- services_context_index.tpl.php
+++ services_context_index.tpl.php
@@ -1,39 +1,48 @@
 <?php
 // $Id$
+drupal_add_css(drupal_get_path('module', 'services') . '/css/admin.css');
 ?>
 <?php foreach ($contexts as $name => $info): ?>
   <div class="services-context">
     <h2><?php print $info['title'] ?></h2>
-    <dl>
-      <dt><?php print t('Path') ?></dt>
-      <dd><?php print $info['path'] ?></dd>
-      <dt><?php print t('Server') ?></dt>
-      <dd><?php print $info['server'] ?></dd>
-      <dt><?php print t('Authentication') ?></dt>
-      <dd><?php print $info['authentication'] ?></dd>
-      <dt><?php print t('Server') ?></dt>
-      <dd><?php print $info['server'] ?></dd>
-      <dt><?php print t('Services') ?></dt>
-      <dd>
-        <ul>
-          <?php foreach ($info['resources'] as $rname => $conf): ?>
-            <li>
-              <?php 
-                if (isset($conf['alias'])) {
-                  print t('!alias (!name)', array(
-                    '!alias' => $conf['alias'],
-                    '!name' => $rname,
-                  ));
-                }
-                else {
-                  print $rname;
-                }
-              ?>
-            </li>
-          <?php endforeach ?>
-        </ul>
-      </dd>
-    </dl>
+    <div class="context-info first">
+      <div class="info">
+        <label><?php print t('Path') ?></label>
+        <span class="value"><?php print $info['path'] ?></span>
+      </div>
+      <div class="info">
+        <label><?php print t('Server') ?></label>
+        <span class="value"><?php print $info['server'] ?></span>
+      </div>
+      <div class="info">
+        <label><?php print t('Authentication') ?></label>
+        <span class="value"><?php print $info['authentication'] ?></span>
+      </div>
+    </div>
+    <div class="context-info last">
+      <div class="info">
+        <label><?php print t('Services') ?></label>
+        <span class="value">
+          <ul>
+            <?php foreach ($info['resources'] as $rname => $conf): ?>
+              <li>
+                <?php 
+                  if (isset($conf['alias'])) {
+                    print t('!alias (!name)', array(
+                      '!alias' => $conf['alias'],
+                      '!name' => $rname,
+                    ));
+                  }
+                  else {
+                    print $rname;
+                  }
+                ?>
+              </li>
+            <?php endforeach ?>
+          </ul>
+        </span>
+      </div>
+    </div>
     <?php
       print l(t('Edit !title', array('!title' => $info['title'],)), 
         'admin/build/services/contexts/' . $name,

d47fcbc9cad890bec66eb52ed41c808217f43e4c
heyrocker <gdd@heyrocker.com>
2010-04-08 17:15:36 -0700

    adding api documentation

---
+++ services.servers.api.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by Services for the definition of servers.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
---
+++ services.services.api.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by Services for the definition of new services.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */

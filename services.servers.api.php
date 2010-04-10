f9e8a721fb6f29d2e9630778880e8c0873545162
heyrocker <gdd@heyrocker.com>
2010-04-10 11:20:35 -0700

    updating api docs

--- services.servers.api.php
+++ services.servers.api.php
@@ -9,3 +9,23 @@
  * @addtogroup hooks
  * @{
  */
+
+/**
+ * Identifies a server implementation to Services.
+ *
+ * @return
+ *   An associative array with the following keys.
+ *
+ *   - name: The display name of this server.
+ *   - path: The path it responds to requests to, relative to webroot.
+ */
+function hook_server_info() {
+  
+}
+
+/**
+ * Acts on requests to the server defined in hook_server_info().
+ */
+function hook_server_server() {
+  
+}
--- services.services.api.php
+++ services.services.api.php
@@ -9,3 +9,16 @@
  * @addtogroup hooks
  * @{
  */
+
+ /**
+  * Defines function signatures for resources available to services.
+  *
+  * Functionally this is very similar to the way hook_menu() works, and in many
+  * ways Services can be seen as an abstraction layer on top of hook_menu().
+  *
+  * @return
+  *   An associative array which defines available resources. 
+  */
+function hook_services_resources() {
+  
+}
